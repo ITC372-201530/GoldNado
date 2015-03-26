dd7dbec0797ce334baf3f83d48f7db1ff720cc48
hyperant <toja.design@gmail.com>
2015-03-26 19:00:18 +1100

    Started working on the building code, using temp user controller for now
    Signed-off-by: Jason

---
+++ Assets/Scripts/PlaceBlock.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlaceBlock : MonoBehaviour {
+	public GameObject goldBrick;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.B)) {
+			print ("Build");
+			this.placeBlock();
+		}
+		
+		if(Input.GetKeyUp(KeyCode.B)) {
+			print ("End Build");
+		}
+	}
+	
+	void placeBlock() {
+		Instantiate(this.goldBrick, this.gameObject.transform.position, this.gameObject.transform.rotation);
+	}
+}
