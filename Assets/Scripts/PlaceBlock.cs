f73c11b4b0b896579f58d9d425b01e1e739ba690
hyperant <toja.design@gmail.com>
2015-03-28 13:03:25 +1100

    fixed up the spawn of the block infront of the camera, add an alpha channel to the new block for when you are placing it. Add block detection so if you are inside another object the block will change its colour in order to indicate this. Add two key inputs in order to rotate the block around the y axis.
    Signed-off-by: Jason

--- Assets/Scripts/PlaceBlock.cs
+++ Assets/Scripts/PlaceBlock.cs
@@ -4,53 +4,106 @@ using System.Collections;
 public class PlaceBlock : MonoBehaviour {
 	public GameObject goldBrick;
 	public GameObject Building;
+	public GameObject camera;
 	
 	private GameObject tmpBlock;
 	
+	private float lockPos = 0;
+	
 	// Use this for initialization
 	void Start () {
-	
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if(Input.GetKeyDown(KeyCode.B)) {
-			print ("Build");
 			this.showBlock();
 		}
 		
 		if(Input.GetKeyUp(KeyCode.B)) {
-			print ("End Build");
 			this.placeBlock();
 		}
 		
-		if(Input.GetAxis("Mouse ScrollWheel") !=0) {
-			print ("Move");
-			this.tmpBlock.transform.Translate(Vector3.forward *Input.GetAxis("Mouse ScrollWheel"));
+		if(Input.GetKey(KeyCode.C)) {
+			if(this.tmpBlock !=null) {
+				Quaternion rot =this.tmpBlock.transform.rotation;
+				rot.y -=0.01f;
+				this.tmpBlock.transform.rotation =rot;
+			}
 		}
+		
+		if(Input.GetKey(KeyCode.V)) {
+			if(this.tmpBlock !=null) {
+				Quaternion rot =this.tmpBlock.transform.rotation;
+				rot.y +=0.01f;
+				this.tmpBlock.transform.rotation =rot;
+			}
+		}
+		
+		if(Input.GetAxis("Mouse ScrollWheel") !=0) {
+			if(this.tmpBlock !=null) {
+				this.tmpBlock.transform.Translate(Vector3.forward *Input.GetAxis("Mouse ScrollWheel"));
+			}
+		}
+		
+		if(Input.GetAxis("Mouse Y") !=0) {
+			//rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+			//rotationY = Mathf.Clamp (rotationY, minimumY, maximumY);
+			if(this.tmpBlock !=null) {
+				Vector3 upP =this.tmpBlock.transform.position;
+				upP.y +=Input.GetAxis("Mouse Y");
+				this.tmpBlock.transform.position =upP;
+			}
+		}
+		
 	}
 	
 	void showBlock() {
-		Vector3 pos =this.gameObject.transform.position;
-		Vector3 dir =this.gameObject.transform.forward;
-		Quaternion rot =this.gameObject.transform.rotation;
+		Vector3 pos =this.camera.transform.position;
+		Vector3 dir =this.camera.transform.forward;
+		Quaternion rot =this.camera.transform.rotation;
+		rot.x =0;
 		float dist =5.0f;
-		
-		pos.y -=0.75f;
 		
 		Vector3 spawnPos =pos +dir *dist;
 		
 		this.tmpBlock =Instantiate(this.goldBrick, spawnPos, rot) as GameObject;
-
-		this.tmpBlock.renderer.material.color -=new Color(0.1f ,0, 0, 0);
 		
+		this.tmpBlock.AddComponent("PlaceBlockDetection");
+		PlaceBlockDetection spt =(PlaceBlockDetection)this.tmpBlock.GetComponent("PlaceBlockDetection");
+		spt.detectionColor =new Color(0.5f, 0, 0, 0.5f);
 		this.tmpBlock.transform.parent =this.transform;
+		
+		this.tmpBlock.transform.rotation =Quaternion.Euler(this.lockPos, this.tmpBlock.transform.rotation.eulerAngles.y, this.lockPos); //In order to stop the block from rotating around to the camera rotation we need to reset the rotation back after we init the block
+		Color col =this.tmpBlock.renderer.material.color;
+		col.a =0.5f;
+		this.tmpBlock.renderer.material.color =col;
 	}
 	
 	void placeBlock() {
-		this.tmpBlock.transform.parent =this.Building.transform;
-		this.tmpBlock.collider.isTrigger =false; //Because we have place the block onto the game scene we no longer want it to just be a trigger
-		this.tmpBlock.collider.attachedRigidbody.useGravity =true; //Allow gravity to affect the block
+		//Before we can place a block we need to make sure it isnt inside another object
+		PlaceBlockDetection spt =(PlaceBlockDetection)this.tmpBlock.GetComponent("PlaceBlockDetection");
+		if(this.tmpBlock.transform.position.y <0 || spt.IsInideObject() ==true) { //Destroy the block
+			Vector3 tmpPos =this.tmpBlock.transform.position;
+			tmpPos.y =-1000;
+			this.tmpBlock.transform.position =tmpPos;
+			
+			Destroy(this.tmpBlock, 0.1f);
+			tmpBlock =null;
+		} else {
+			this.tmpBlock.transform.parent =this.Building.transform;
+			this.tmpBlock.collider.isTrigger =false; //Because we have place the block onto the game scene we no longer want it to just be a trigger
+			this.tmpBlock.collider.attachedRigidbody.useGravity =true; //Allow gravity to affect the block
+			Color col =this.tmpBlock.renderer.material.color;
+			col.a =1;
+			this.tmpBlock.renderer.material.color =col;
+			//Destroy(this.tmpBlock.GetComponent("PlaceBlockDetection"));
+			
+			spt.detectionColor =new Color(0.5f, 0.5f, 0);
+			spt.col =col;
+			
+			this.tmpBlock =null;
+		}
 	}
 	
 }
