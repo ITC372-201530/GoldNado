7cf03a83b2dcc58243a34af048e1a563d5d0b3b9
hyperant <toja.design@gmail.com>
2015-04-01 17:35:28 +1100

    Added in a shadow affect for when placing a brick, makes it a lot easier to see where the brick is going to end up
    Signed-off-by: hyperant <toja.design@gmail.com>

--- Assets/Scripts/PlaceBlock.cs
+++ Assets/Scripts/PlaceBlock.cs
@@ -5,8 +5,10 @@ public class PlaceBlock : MonoBehaviour {
 	public GameObject goldBrick;
 	public GameObject Building;
 	public GameObject camera;
+	public GameObject shadow;
 	
 	private GameObject tmpBlock;
+	private GameObject tmpShadow;
 	
 	private float lockPos = 0;
 	
@@ -59,6 +61,10 @@ public class PlaceBlock : MonoBehaviour {
 	}
 	
 	void showBlock() {
+		Vector3 sPos =new Vector3(0,0,0);
+		Quaternion sRot =Quaternion.identity;
+		//sRot.x =90;
+		
 		Vector3 pos =this.camera.transform.position;
 		Vector3 dir =this.camera.transform.forward;
 		Quaternion rot =this.camera.transform.rotation;
@@ -68,6 +74,8 @@ public class PlaceBlock : MonoBehaviour {
 		Vector3 spawnPos =pos +dir *dist;
 		
 		this.tmpBlock =Instantiate(this.goldBrick, spawnPos, rot) as GameObject;
+		this.tmpShadow =Instantiate(this.shadow, spawnPos, this.shadow.transform.rotation) as GameObject;
+		this.tmpShadow.transform.parent =this.tmpBlock.transform;
 		
 		this.tmpBlock.AddComponent("PlaceBlockDetection");
 		PlaceBlockDetection spt =(PlaceBlockDetection)this.tmpBlock.GetComponent("PlaceBlockDetection");
@@ -77,13 +85,14 @@ public class PlaceBlock : MonoBehaviour {
 		//GameObject child =this.tmpBlock.transform.Find("goldBrick").gameObject;
 		
 		this.tmpBlock.transform.rotation =Quaternion.Euler(this.lockPos, this.tmpBlock.transform.rotation.eulerAngles.y, this.lockPos); //In order to stop the block from rotating around to the camera rotation we need to reset the rotation back after we init the block
-		child.transform.rotation =Quaternion.Euler(this.lockPos, this.tmpBlock.transform.rotation.eulerAngles.y, this.lockPos); //In order to stop the block from rotating around to the camera rotation we need to reset the rotation back after we init the block
+		//child.transform.rotation =Quaternion.Euler(this.lockPos, this.tmpBlock.transform.rotation.eulerAngles.y, this.lockPos); //In order to stop the block from rotating around to the camera rotation we need to reset the rotation back after we init the block
 		Color col =this.tmpBlock.renderer.material.color;
 		col.a =0.5f;
 		this.tmpBlock.renderer.material.color =col;
 	}
 	
 	void placeBlock() {
+		Destroy(this.tmpShadow);
 		//Before we can place a block we need to make sure it isnt inside another object
 		PlaceBlockDetection spt =(PlaceBlockDetection)this.tmpBlock.GetComponent("PlaceBlockDetection");
 		if(this.tmpBlock.transform.position.y <0 || spt.IsInideObject() ==true) { //Destroy the block
